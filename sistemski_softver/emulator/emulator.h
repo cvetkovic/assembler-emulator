96d85a23c7ffe663c7ac643b15891823566f7f34
Lazar Cvetković <jugosoft@hotmail.com>
2019-05-16 01:10:30 +0200

    emulator and linker classes made

---
+++ sistemski_softver/emulator/emulator.h
@@ -0,0 +1,9 @@
+#ifndef _EMULATOR_EMULATOR_H
+#define _EMULATOR_EMULATOR_H
+
+class Emulator
+{
+
+};
+
+#endif
---
+++ sistemski_softver/emulator/linker.h
@@ -0,0 +1,9 @@
+#ifndef _LINKER_EMULATOR_H
+#define _LINKER_EMULATOR_H
+
+class Emulator
+{
+
+};
+
+#endif
