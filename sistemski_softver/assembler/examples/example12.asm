ea4d2deca82ebe5ce83e3146129f4818fad4f765
Lazar M. Cvetković <l.cvetkovic.997@gmail.com>
2019-06-09 13:35:44 +0200

    example change

--- sistemski_softver/assembler/examples/example12.asm
+++ sistemski_softver/assembler/examples/example12.asm
@@ -1,18 +1,8 @@
 .data
-
-pocetak:
+.global pga
 .word 0xabcd
-
-.equ diff, kraj-_start
-
-.text
-.global _start
-
-_start:
-add r0, &diff
-kraj:
-jmp _start
-
-
-.section sekcija
-halt
+pga:
+.word pga
+pga1:
+.word pga1
+.equ razlika, pga1-pga
